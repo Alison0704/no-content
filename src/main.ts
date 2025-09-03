1723e901b62104a1c63aa174df253d3c1989aecf
Alison Emilien <aemil072@uottawa.ca>
2025-09-02 20:05:09 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  <img src="src/assets/Logo.png" alt="icon"></img>
+  <img src="./assets/Logo.png" alt="icon">
    <h3>Hey...I'm still working on it</h3>
    <p>Please come back later</p>
   </div>

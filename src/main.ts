da89907a601868bb949a8367588a37ffa070d183
Alison Emilien <aemil072@uottawa.ca>
2025-09-03 00:55:52 -0400

    making everything base vanilla setting

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  <img src="./assets/Logo.png" alt="icon">
-   <h3>Hey...I'm still working on it</h3>
-   <p>Please come back later</p>
-  </div>
-`

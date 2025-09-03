68509767c44a5627c380e4d5492656a92c4f643b
Alison Emilien <aemil072@uottawa.ca>
2025-09-02 20:00:56 -0400

    first and hopefully last commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  <img src="src/assets/Logo.png" alt="icon"></img>
+   <h3>Hey...I'm still working on it</h3>
+   <p>Please come back later</p>
+  </div>
+`
